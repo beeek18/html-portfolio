bbb8af75810f87c2281ba65e0328950bdbc0cfe1
Bekzodbek <kodirov2000@list.ru>
2023-02-10 01:05:59 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+//1.  check dark mode on system settings
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  btnDarkMode.classList.add('dark-mode-btn--active')
+  document.body.classList.add('dark')
+}
+
+
+//2.  check dark mode in localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+  btnDarkMode.classList.add('dark-mode-btn--active')
+  document.body.classList.add('dark')
+} else if (localStorage.getItem('darkMode') === 'light') {
+  btnDarkMode.classList.remove('dark-mode-btn--active')
+  document.body.classList.remove('dark')
+}
+
+
+//3.if change system settings, change theme
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', (event) => {
+  const newColorScheme = event.matches ? 'dark' : 'light';
+
+  if (newColorScheme === 'dark') {
+    btnDarkMode.classList.add('dark-mode-btn--active')
+    document.body.classList.add('dark')
+    localStorage.setItem('darkMode', 'dark')
+  } else {
+    btnDarkMode.classList.remove('dark-mode-btn--active')
+    document.body.classList.remove('dark')
+    localStorage.setItem('darkMode', 'light')
+  }
+
+})
+
+
+
+
+//on/of dark mode
+btnDarkMode.onclick = function () {
+  btnDarkMode.classList.toggle("dark-mode-btn--active");
+  const isDark = document.body.classList.toggle("dark")
+
+  if (isDark) {
+    localStorage.setItem('darkMode', 'dark')
+  } else {
+    localStorage.setItem('darkMode', 'light')
+  }
+}
